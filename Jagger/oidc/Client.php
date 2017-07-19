9c75b9d387cf8acab9f33580faeb6dd306f387a6
Janusz Ulanowski <janusz.ulanowski@heanet.ie>
2017-07-19 10:21:46 +0100

    minor fixes

--- Jagger/oidc/Client.php
+++ Jagger/oidc/Client.php
@@ -1,9 +1,10 @@
 <?php
+
 namespace Jagger\oidc;
 
 use Lcobucci;
 
-class Client
+class Client extends Lcobucci\JWT\Token
 {
     private $sessionFlowId = 'joidc';
 
@@ -13,33 +14,39 @@ class Client
     private $rpRedirectURL;
     private $opConfig = array();
     private $scopes = array('openid');
+    private $access_token;
     protected $configuration;
     protected $jwks;
     protected $authzparams = array();
     const OPCONFURI = '/.well-known/openid-configuration';
 
 
-    public function __construct(array $openidconf = null) {
+    public function __construct(array $openidconf = null)
+    {
         if (is_array($openidconf)) {
             $this->opConfig = $openidconf;
         }
 
     }
 
-    public function setProviderURL($opURL) {
+    public function setProviderURL($opURL)
+    {
         $this->opConfig['issuer'] = $opURL;
         $_SESSION['joidc_issuer'] = $opURL;
     }
 
-    public function setClientSecret($clientSecret) {
+    public function setClientSecret($clientSecret)
+    {
         $this->clientSecret = $clientSecret;
     }
 
-    public function setClientID($clientID) {
+    public function setClientID($clientID)
+    {
         $this->clientID = $clientID;
     }
 
-    public function generateRandStr() {
+    public function generateRandStr()
+    {
         $randstr = sha1(openssl_random_pseudo_bytes(1024));
         return $randstr;
     }
@@ -47,27 +54,32 @@ class Client
     /**
      * @param array $params
      */
-    public function addAuthzParams(array $params) {
+    public function addAuthzParams(array $params)
+    {
         $this->authzparams = array_merge($params);
     }
 
-    public function getAutzParams() {
+    public function getAutzParams()
+    {
         return $this->authzparams;
     }
 
-    public function addScope($scope) {
+    public function addScope($scope)
+    {
         $this->scopes = array_merge($this->scopes, (array)$scope);
         $this->scopes = array_unique($this->scopes);
     }
 
-    public function setRedirectURL($url) {
+    public function setRedirectURL($url)
+    {
         if (filter_var($url, FILTER_VALIDATE_URL) !== false) {
             $this->rpRedirectURL = $url;
         }
     }
 
 
-    public function getProviderURL() {
+    public function getProviderURL()
+    {
         if (!isset($this->opConfig['issuer'])) {
             throw new \Exception('The provider URL has not been set');
         } else {
@@ -75,14 +87,16 @@ class Client
         }
     }
 
-    public function getScopes() {
+    public function getScopes()
+    {
         return $this->scopes;
     }
 
-    public function getOPConfigValue($param) {
+    public function getOPConfigValue($param)
+    {
         if (!isset($this->opConfig['' . $param . ''])) {
             $wellKnownConfigUrl = rtrim($this->getProviderURL(), '/') . self::OPCONFURI;
-            $value = json_decode($this->runHttpRequest($wellKnownConfigUrl), null)->{$param};
+            $value = json_decode($this->runHttpRequest($wellKnownConfigUrl, array(), null), null)->{$param};
             if ($value) {
                 $this->opConfig[$param] = $value;
             } else {
@@ -93,49 +107,57 @@ class Client
         return $this->opConfig[$param];
     }
 
-    public function getOPConfiguration() {
+    public function getOPConfiguration()
+    {
 
     }
 
-    public function getRedirectURL() {
+    public function getRedirectURL()
+    {
         return $this->rpRedirectURL;
     }
 
 
-    public function setOnceSession($once = null) {
+    public function setOnceSession($once = null)
+    {
         if ($once === null) {
             $once = $this->generateRandStr();
         }
         $_SESSION['joidc_once'] = $once;
     }
 
-    public function setStateSession($state = null) {
+    public function setStateSession($state = null)
+    {
         if ($state === null) {
             $state = $this->generateRandStr();
         }
         $_SESSION['joidc_state'] = $state;
     }
 
-    private function getStateSession() {
+    private function getStateSession()
+    {
         if (!isset($_SESSION['joidc_state'])) {
             $this->setStateSession();
         }
         return $_SESSION['joidc_state'];
     }
 
-    private function getOnceSession() {
+    private function getOnceSession()
+    {
         if (!isset($_SESSION['joidc_once'])) {
             $this->setOnceSession();
         }
         return $_SESSION['joidc_once'];
     }
 
-    private function getLoginHint() {
+    private function getLoginHint()
+    {
         return null;
     }
 
 
-    public function generateAuthzRequest($responseType = 'code') {
+    public function generateAuthzRequest($responseType = 'code')
+    {
         $issuerURL = $this->getProviderURL();
         $authzURL = $this->getOPConfigValue('authorization_endpoint');
         $params = array(
@@ -154,25 +176,38 @@ class Client
     }
 
 
-    private function runHttpRequest($url, $postBody = null) {
+    /**
+     * @param $url
+     * @param array $headers
+     * @param $postBody
+     * @return mixed
+     * @throws \Exception
+     */
+    private function runHttpRequest($url, array $headers, $postBody)
+    {
+        $inHeaders = array();
         $curl = curl_init();
-        if ($postBody != null) {
+        if ($postBody !== null) {
             curl_setopt($curl, CURLOPT_POST, 1);
             curl_setopt($curl, CURLOPT_POSTFIELDS, $postBody);
             $content_type = 'application/x-www-form-urlencoded';
             if (is_object(json_decode($postBody))) {
                 $content_type = 'application/json';
             }
-            curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+            $inHeaders = array(
                 "Content-Type: {$content_type}",
-                'Content-Length: ' . strlen($postBody)
-            ));
+                'Content-Length: ' . strlen($postBody),
+            );
         }
+        foreach ($headers as $k => $v) {
+            $inHeaders[] = trim('' . $k . ': ' . $v . '');
+        }
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $inHeaders);
         curl_setopt($curl, CURLOPT_URL, $url);
         if (isset($this->httpProxy)) {
             curl_setopt($curl, CURLOPT_PROXY, $this->httpProxy);
         }
-        curl_setopt($curl, CURLOPT_HEADER, 0);
+
         if (isset($this->certPath)) {
             curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
             curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
@@ -190,13 +225,15 @@ class Client
         return $output;
     }
 
-    public function redirect($url) {
+    public function redirect($url)
+    {
         header('Location: ' . $url);
         exit;
     }
 
 
-    public function authenticate() {
+    public function authenticate()
+    {
         // parse response
         $code = null;
         $state = null;
@@ -218,14 +255,18 @@ class Client
         if ($code === null) {
             throw new \Exception('"code" param has not been received from Authorization Server');
         }
-        $authorizationCode = $this->requestTokens($code);
-        if (isset($authorizationCode->error)) {
-            throw new \Exception($authorizationCode->error_description);
+        $token = $this->requestTokens($code);
+
+        if (isset($token->error)) {
+            throw new \Exception($token->error_description);
         }
-        if (!isset($authorizationCode->id_token)) {
+        if(!isset($token->access_token)){
+             throw new \Exception('access_token has not been received from Authorization Server');
+        }
+        if (!isset($token->id_token)) {
             throw new \Exception('id_token has not been received from Authorization Server');
         }
-        $tmpimpl = count(explode('.', $authorizationCode->id_token));
+        $tmpimpl = count(explode('.', $token->id_token));
         if ($tmpimpl != 3 && $tmpimpl != 5) {
             throw new \Exception('Incorrect id_token received from Authorization Server');
         }
@@ -234,27 +275,48 @@ class Client
         }
 
 
+
         $parser = new Lcobucci\JWT\Parser();
-        $token = $parser->parse($authorizationCode->id_token);
-        $alg = $token->getHeader('alg');
+        $id_token = $parser->parse($token->id_token);
+        $this->access_token = $token->access_token;
+
+
+
+
+        $alg = $id_token->getHeader('alg');
+
         if ($alg !== 'RS256') {
             throw new \Exception('Only alg RS256 is accepted');
         }
-        $kid = $token->getHeader('kid');
+        $kid = null;
+        try {
+            $kid = $id_token->getHeader('kid');
+        } catch (\OutOfBoundsException $e) {
+            log_message('debug', 'kid not found');
+        }
 
+
+        /**
+         * need to use sleep 1 sec; third lib using (int) to compare iat claim
+         */
+        sleep(1);
         $validationData = new Lcobucci\JWT\ValidationData();
 
         $validationData->setIssuer($this->getProviderURL());
-        $validationData->setAudience($this->clientID);
+        //$validationData->setAudience($this->clientID);
+        /**
+         * @todo workournd about aud
+         */
 
-        $isValidToken = $token->validate($validationData);
+        $isValidToken = $id_token->validate($validationData);
+
         if (!$isValidToken) {
             throw new \Exception('Received "id_token" is not valid - propbably expired');
         }
         // verify sig
 
         $jwks_uri = $this->getOPConfigValue('jwks_uri');
-        $jwks_body = $this->runHttpRequest($jwks_uri);
+        $jwks_body = $this->runHttpRequest($jwks_uri, array(), null);
 
         $this->jwks = json_decode($jwks_body, true);
 
@@ -263,14 +325,27 @@ class Client
         }
 
         $keyVer = null;
-        foreach ($this->jwks['keys'] as $key => $val) {
+        if ($kid !== null) {
+            foreach ($this->jwks['keys'] as $key => $val) {
 
-            if ($val['kid'] === $kid && $val['use'] === 'sig') {
-                $keyVer = $this->jwks['keys'][$key];
-                break;
+                if (isset($val['kid']) && $val['kid'] === $kid && $val['use'] === 'sig') {
+                    $keyVer = $this->jwks['keys'][$key];
+                    break;
+                }
+
             }
-
+        } else {
+            foreach ($this->jwks['keys'] as $key => $val) {
+                /**
+                 * use first found sig
+                 */
+                if (isset($val['use']) && $val['use'] === 'sig') {
+                    $keyVer = $this->jwks['keys'][$key];
+                    break;
+                }
+            }
         }
+
         if ($keyVer === null) {
             throw new \Exception('JWK not found, cannot verify signature');
         }
@@ -279,7 +354,7 @@ class Client
 
         $signer = new Lcobucci\JWT\Signer\Rsa\Sha256();
         $keychain = new Lcobucci\JWT\Signer\Keychain();
-        $sigValid = $token->verify($signer, $keychain->getPublicKey($jwkObj->toKey()));
+        $sigValid = $id_token->verify($signer, $keychain->getPublicKey($jwkObj->toKey()));
 
         if ($sigValid !== true) {
             throw new \Exception('Received "id_token" is not valid. Signature validation failed');
@@ -287,7 +362,7 @@ class Client
         /**
          * @var Lcobucci\JWT\Claim\Basic[] $claimsObj
          */
-        $claimsObj = $token->getClaims();
+        $claimsObj = $id_token->getClaims();
         $claims = array();
         foreach ($claimsObj as $cl) {
 
@@ -296,31 +371,68 @@ class Client
             }
 
         }
-        $claims['iss'] = $token->getClaim('iss');
+        $claims['iss'] = $id_token->getClaim('iss');
         unset($_SESSION['joidc_once']);
         unset($_SESSION['joidc_state']);
         unset($_SESSION['joidc_issuer']);
+
+
         return $claims;
 
     }
 
 
-    private function requestTokens($code) {
+    private function requestTokens($code)
+    {
         $tokenEndpoint = $this->getOPConfigValue('token_endpoint');
-        $token_params = array(
-            'grant_type' => 'authorization_code',
-            'code' => $code,
-            'redirect_uri' => $this->getRedirectURL(),
-            'client_id' => $this->clientID,
-            'client_secret' => $this->clientSecret
-        );
+        $authMethod = 'client_secret_basic';
+        $authMethodConf = $this->getOPConfigValue('token_endpoint_auth_methods_supported');
+        if (!is_array($authMethodConf)) {
+            $authMethod = 'client_secret_basic';
+        } elseif (!in_array($authMethod, $authMethodConf, true) && in_array('client_secret_post', $authMethodConf, true)) {
+
+            $authMethod = 'client_secret_post';
+        }
+
+
+        if ($authMethod === 'client_secret_post') {
+            $token_params = array(
+                'grant_type' => 'authorization_code',
+                'code' => $code,
+                'redirect_uri' => $this->getRedirectURL(),
+                'client_id' => $this->clientID,
+                'client_secret' => $this->clientSecret
+            );
+            $headers = array();
+        } else {
+            $token_params = array(
+                'grant_type' => 'authorization_code',
+                'code' => $code,
+                'redirect_uri' => $this->getRedirectURL(),
+                'client_id' => $this->clientID
+            );
+            $headers = array('Authorization' => 'Basic ' . $this->urlsafeB64Encode($this->clientID . ':' . $this->clientSecret) . '');
+        }
+
         // Convert token params to string format
         $token_params = http_build_query($token_params, null, '&');
-        return json_decode($this->runHttpRequest($tokenEndpoint, $token_params));
+        return json_decode($this->runHttpRequest($tokenEndpoint, $headers, $token_params));
+    }
+
+    public function requestUserinfo(){
+        $url = $this->getOPConfigValue('userinfo_endpoint');
+        $headers = array(
+            'Authorization' => 'Bearer '. $this->access_token.'',
+        );
+        return json_decode($this->runHttpRequest($url, $headers, null));
+
+
+
     }
 
 
-    private function urlsafeB64Decode($input) {
+    private function urlsafeB64Decode($input)
+    {
         $remainder = strlen($input) % 4;
         if ($remainder) {
             $padlen = 4 - $remainder;
@@ -329,7 +441,8 @@ class Client
         return base64_decode(strtr($input, '-_', '+/'));
     }
 
-    private function urlsafeB64Encode($input) {
+    private function urlsafeB64Encode($input)
+    {
         return str_replace('=', '', strtr(base64_encode($input), '+/', '-_'));
     }
 }
